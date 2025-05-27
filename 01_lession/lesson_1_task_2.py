8b16d674f4fe21d7cdcd4d4054deed3402e51a01
Mihail Bondarenko <vampnnight674@gmail.com>
2025-05-27 23:39:56 +0300

    дополнения

--- 01_lession/lesson_1_task_2.py
+++ 01_lession/lesson_1_task_2.py
@@ -1,2 +1,3 @@
 my_age = (54)
+my_age = (57)
 print(my_age)
--- 01_lession/lesson_1_task_3.py
+++ 01_lession/lesson_1_task_3.py
@@ -1,3 +1,3 @@
 first_name = input("Как Ваше имя?")
 last_name = input("Как Ваша фамилия?")
-print("Вас зовут:"+ first_name +last_name)
+print("Вас зовут:"+ first_name+ ' ' +last_name)
